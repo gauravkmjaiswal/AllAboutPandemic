2fe5ba0951b1af10128064fbc95cfe04c288b7da
Vishal Jaiswal <gauravkmjaiswal.com>
2022-10-02 14:46:04 +0530

    2.1.3

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-
 //components
 import Connection from './database/db.js';
 import Router from './routes/route.js';
-
+const path =require('path')
 
 dotenv.config();
 
@@ -17,9 +16,14 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', Router);
 
+
 if(process.env.NODE_ENV=='production')
 {
-    app.use(express.static("client/build"))
+
+    app.get('/',(req,res)=>{
+        app.use(express.static(path.resolve(__dirname,'client','build','index.html')))
+        app.sendfile(path.resolve(__dirname,'client','build','index.html'))
+    })
 }
 
 const PORT = process.env.Port || 8000;
