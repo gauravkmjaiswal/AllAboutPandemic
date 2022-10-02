2f16bf7221e34afd2c83a81c6c43859502b8fd8e
Vishal Jaiswal <gauravkmjaiswal.com>
2022-10-02 13:10:23 +0530

    2.1

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+//components
+import Connection from './database/db.js';
+import Router from './routes/route.js';
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', Router);
+
+if(process.env.NODE_ENV=='production')
+{
+    app.use(express.static("client/build"))
+}
+
+const PORT = process.env.Port || 8000;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+
+const URL = process.env.MONGODB_URI || `mongodb+srv://${username}:${codeforinterview}@allaboutpendemic.asqmc.mongodb.net/AllAboutPendemic?retryWrites=true&w=majority`;
+Connection(URL);
